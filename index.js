aa16fcb6f28143240bf75d8c64a315a5fb299bac
Ryan <poderosochefinho218@gmail.com>
2026-03-16 19:53:28 -0300

    Log success message for slash command registration

--- index.js
+++ index.js
@@ -502,6 +502,8 @@ client.once("clientReady", async () => {
     const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] });
     await rest.put(Routes.applicationGuildCommands(CLIENT_ID, "1125234022432383037"), { body: slashCommands });
+    console.log("Slash commands registrados no servidor.");
+  } catch (err) {
     console.error("[slash] Erro ao registrar slash commands:", err);
   }
 });
